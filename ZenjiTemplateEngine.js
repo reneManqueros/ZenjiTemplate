498a8b06d196c5de869954829339cc29700cac52
Rene Manqueros <rene@manqueros.com>
2012-04-03 14:22:39 -0600

    Initial version

---
+++ ZenjiTemplateEngine.js
@@ -0,0 +1,19 @@
+(function( $, window, document, undefined ) {
+    $.fn.ZenjiTemplate = function(dataSource){
+        var template = $.trim(this.html());
+        var dataSourceSize = $(dataSource).size();
+        var loopedTemplatedText = '';
+        for(var i =0;i<dataSourceSize;i++){
+            elem = dataSource[i];
+            var loopedTemplate = template;
+            for(propertyName in elem){
+                loopedTemplate = loopedTemplate.replace(new RegExp('{{'+propertyName+'}}','g'),elem[propertyName]);
+
+            }
+            loopedTemplatedText += loopedTemplate;
+        }
+        return $(loopedTemplatedText);
+
+    };
+
+})( jQuery, window, document );
